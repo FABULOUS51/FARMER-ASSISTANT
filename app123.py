fbbf07778b90c5bdd45fed723dc7088f15eca091
FABULOUS51 <alfredwilson7131@gmail.com>
2025-03-06 22:18:18 +0530

    Update app123.py

--- app123.py
+++ app123.py
@@ -59,7 +59,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.image(r"360_F_123708977_X8lHoZ3iSb6rRjsmFb2mxGNp2dngJrjh.jpg",use_container_width=True)
 with col2:
-    st.image(r"",use_container_width=True)
+    st.image(r"5e7c07a78fb76a9066bbfa410458b849.jpg",use_container_width=True)
 with col3:
     st.image(r"C:\Users\edwin\Desktop\alfred\farmers\lovepik-farmer-farming-in-wheat-field-picture_501611486.jpg",use_container_width=True)
 st.write("**UPLOAD SOIL IMAGES FOR CROP RECOMMENDATION**.")
